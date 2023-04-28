f0f50ba922e436caf17d51635dbc65d574cbeedd
unknown <you@example.com>
2023-04-28 17:21:14 -0300

    app filmes

---
+++ src/components/BannerMovies/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+
+    TextBanner:{
+        fontSize:30,
+        color:"white",
+        marginTop:15,
+        marginLeft:-180,
+        fontWeight:'Bold'
+    },
+    ImageBanner:{
+        width:'100%',
+        height:800,
+        marginTop:15,
+        borderRadius:10
+    }
+
+})
+export default styles;
