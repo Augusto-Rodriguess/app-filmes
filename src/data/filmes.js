22ea80ee96742863fbe836fb278d692d4aa505b3
unknown <you@example.com>
2023-05-05 17:16:18 -0300

    add flatlist

---
+++ src/data/filmes.js
@@ -0,0 +1,65 @@
+const DATA = 
+[
+    {
+        id:'01',
+        nome:'Adão Negro',
+        nota:'4.5',
+        imagem:'adao.jpg'
+    },
+    {
+        id:'02',
+        nome:'Avatar',
+        nota:'4.7',
+        imagem:'avatar.jpg'
+    },
+    {
+        id:'03',
+        nome:'Homem Formiga',
+        nota:'1.7',
+        imagem:'homemformiga.jpg'
+    },
+    {
+        id:'04',
+        nome:'Desaparecida',
+        nota:'4.0',
+        imagem:'desaparecida.jpg'
+    },
+    {
+        id:'05',
+        nome:'Incríveis',
+        nota:'4.6',
+        imagem:'incriveis.jpg'
+    },
+    {
+        id:'06',
+        nome:'Mario(aquele lá)',
+        nota:'4.9',
+        imagem:'mario.jpg'
+    },
+    {
+        id:'07',
+        nome:'Megan',
+        nota:'2.8',
+        imagem:'megan.jpg'
+    },
+    {
+        id:'08',
+        nome:'Pânico',
+        nota:'5.0',
+        imagem:'panico.jpg'
+    },
+    {
+        id:'09',
+        nome:'Wandinha',
+        nota:'4.1',
+        imagem:'wandinha.jpg'
+    },
+    {
+        id:'10',
+        nome:'Gato two',
+        nota:'5.0',
+        imagem:'gato.jpg'
+    },
+]
+
+export default DATA;
